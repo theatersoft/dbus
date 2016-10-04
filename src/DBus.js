01819f69009cd8517ad144d57077202196ab13c8
Rob Huehn <rhuehn@gmail.com>
2016-10-03 17:37:26 -0700

    initial implementation

--- src/DBus.js
+++ src/DBus.js
@@ -4,6 +4,31 @@ const
     _dbus = new __dbus(),
     _bus = _dbus.getBus('system')
 
+const
+    getInterface = ({service, path, name}) => new Promise((r, j) =>
+        _bus.getInterface(service, path, name, (err, intf) => {
+            if (err) j(err)
+            else r(intf)
+        })),
+    getProperty = (_intf, property) => new Promise((r, j) =>
+        _intf.getProperty(property, (err, value) => {
+            if (err) j(err)
+            else r(value)
+        })),
+    setProperty = (_intf, property, value) => new Promise((r, j) =>
+        _intf.setProperty(property, value, err => {
+            if (err) j(err)
+            else r()
+        })),
+    callMethod = (_intf, method, args) => {
+        method = _intf[method] //// TODO
+        return new Promise((r, j) => {
+            method.finish = ret => r(ret)
+            method.error = err => j(err)
+            method.apply(intf, args)
+        })
+    }
+
 export default class DBus {
     constructor (bus) {
         this.bus = bus
@@ -13,7 +38,21 @@ export default class DBus {
         return this.bus.registerObject('DBus', this)
     }
 
-    ping () {
-        return 'ping'
+    getInterface (service, path, name) {
+        const _intf = getInterface(service, path, name)
+        //TODO introspection
+    }
+
+    getProperty (intf, property) {
+        console.log('getProperty', intf, property)
+        return getInterface(intf).then(_intf => getProperty(_intf, property))
+    }
+
+    setProperty (intf, property, value) {
+        return getInterface(intf).then(_intf => setProperty(_intf, property, value))
+    }
+
+    callMethod (intf, method, args) {
+        return getInterface(intf).then(_intf => callMethod(_intf, method, args))
     }
 }
--- test/test.js
+++ test/test.js
@@ -2,13 +2,26 @@
 
 const
     {Bus} = require('@theatersoft/bus'),
-    DBus = Bus.proxy('DBus')
+    DBus = Bus.proxy('DBus'),
+    networkManager = {
+        service: 'org.freedesktop.NetworkManager',
+        path: '/org/freedesktop/NetworkManager',
+        name: '/org/freedesktop/NetworkManager'
+    }
 
-Bus.start().then((bus) => {
-    DBus.ping()
-        .then(res => {
-            console.log('DBus.ping returned', res)
-        })
+Bus.start().then(_ => {
+    DBus.getProperty(networkManager, 'NetworkingEnabled')
+        .then(res =>
+            console.log('1. getProperty NetworkingEnabled returned', res))
+        .catch(err =>
+            console.log('1. getProperty NetworkingEnabled rejected', err))
 })
 
-
+/*
+ dbus-send --system --print-reply \
+ >             --dest=org.freedesktop.NetworkManager \
+ >             /org/freedesktop/NetworkManager \
+ >             org.freedesktop.DBus.Properties.Get \
+ >             string:"org.freedesktop.NetworkManager" \
+ >             string:"NetworkingEnabled"
+ */
